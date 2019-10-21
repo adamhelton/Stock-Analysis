35ec89e74e37371386ff1647838474830d4d940f
Adam Helton <helton93@protonmail.com>
2019-10-21 19:17:31 -0400

    imported time and set df  with start and end

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from pandas_datareader import data
+import datetime 
+
+start = datetime.datetime(2019, 10, 14)
+end = datetime.datetime(2019, 10, 25)
+df = data.DataReader(name = "AAPL", data_source= 'yahoo', start = start, end =end)
+
+print(df)
